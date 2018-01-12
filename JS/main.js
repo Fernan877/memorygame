93b9213ee4d86502cb32eb57648eed0ba54355d6
Fernando Gonzalez <fernan@Fernandos-Air.fios-router.home>
2018-01-12 16:41:55 -0500

    created game logic

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardONe = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+console.log("User flipped " + cardONe);
+console.log("User flipped " + cardTwo);
